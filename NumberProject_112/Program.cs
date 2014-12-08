e717fe6990f72abf1c75211243d7b92b82aee181
TarfaM <tarfa.mansour@gmail.com>
2014-12-08 22:36:44 +0300

    first publish

---
+++ NumberProject_112/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberProject_112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123456");
+            Console.Read();
+        }
+    }
+}
